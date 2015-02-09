1c00fa68bd3e2f72b757a585eea380554bd5db88
Melanie <melanierod@gmail.com>
2015-02-09 22:21:40 +0000

    Unique Characters

--- java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
+++ java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
@@ -2,12 +2,20 @@ package info.technicallycorrect.qi;
 
 public class ArraysAndStrings {
 
-  public static boolean uniqueChars(final String input) {
-    return false;
-  }
+    public static boolean uniqueChars(final String input) {
+        //Will work only if string is ASCII
+        boolean[] bool_char = new boolean[256];
+        for (int i = 0; i < input.length(); i++) {
+            int val = input.charAt(i);
+            if (bool_char[val])
+                return false;
+            bool_char[val] = true;
+        }
+        return true;
+    }
 
-  public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
-    return false;
-  }
+    public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
+        return false;
+    }
 
 }
--- java/src/test/java/info/technicallycorrect/qi/ArraysAndStringsTest.java
+++ java/src/test/java/info/technicallycorrect/qi/ArraysAndStringsTest.java
@@ -11,14 +11,9 @@ public class ArraysAndStringsTest {
 
   @Test
   public void uniqueChars() {
-    assertTrue(assertUniqueEquals("Hwasung"));
-    assertFalse(assertUniqueEquals("Melanie"));
-  }
-  
-  private static boolean assertUniqueEquals(final String input) {
-    assertEquals(ArraysAndStrings.uniqueChars(input),
-                 ArraysAndStrings.uniqueCharsWithoutExtraDataStructure(input));
-    return ArraysAndStrings.uniqueChars(input);
+    assertFalse(ArraysAndStrings.uniqueChars("Melanie"));
+    assertTrue(ArraysAndStrings.uniqueChars("Hwasung"));
+
   }
 
 }
