d3136f8e32144516bec9a8d73f1703017b5a27cd
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2015-02-19 21:53:55 +0000

    Switching to 4 space indentation

--- java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
+++ java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
@@ -2,17 +2,17 @@ package info.technicallycorrect.qi;
 
 public class ArraysAndStrings {
 
-  public static boolean uniqueChars(final String input) {
-    return false;
-  }
+    public static boolean uniqueChars(final String input) {
+        return false;
+    }
 
-  public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
-    return false;
-  }
+    public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
+        return false;
+    }
 
-  // Given a C-style string that terminates with \*, reverse it.
-  public static String reverse(final String input) {
-    return "";
-  }
+    // Given a C-style string that terminates with \*, reverse it.
+    public static String reverse(final String input) {
+        return "";
+    }
 
 }
