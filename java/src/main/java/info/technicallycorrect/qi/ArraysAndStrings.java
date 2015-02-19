a6294c2cf5d819b897c9e1add6ae21a118a90803
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2015-02-19 21:49:40 +0000

    4 space indentation

--- java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
+++ java/src/main/java/info/technicallycorrect/qi/ArraysAndStrings.java
@@ -2,12 +2,12 @@ package info.technicallycorrect.qi;
 
 public class ArraysAndStrings {
 
-  public static boolean uniqueChars(final String input) {
-    return false;
-  }
+    public static boolean uniqueChars(final String input) {
+        return false;
+    }
 
-  public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
-    return false;
-  }
+    public static boolean uniqueCharsWithoutExtraDataStructure(final String input) {
+        return false;
+    }
 
 }
