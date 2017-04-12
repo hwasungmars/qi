8c10cee82e12078097413c009d4c9fe153ac38ca
Hwasung Lee <hwasung_lee@apple.com>
2017-04-12 21:41:05 +0100

    Update to Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject qi "0.1.0-SNAPSHOT"
   :description "Quite Interesting questions."
   :dependencies [[org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/clojure "1.6.0"]])
+                 [org.clojure/clojure "1.8.0"]])
